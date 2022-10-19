05f7f72e8acbc4d71c0ee54b2fe979eeb87ed80a
tarikul12 <114867607+tarikul12@users.noreply.github.com>
2022-10-19 20:08:19 +0600

    Add files via upload

---
+++ insertion sort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = { 13,2,4,56,67 };
+    int N = sizeof(arr) / sizeof(arr[0]);
+    cout << "Sorted array: \n";
+    insertionSort(arr, N);
+    printArray(arr, N);
+
+    return 0;
+}
