25e3e4d726afe1216681c91d7c4d457b4dafb2e5
tarikul12 <114867607+tarikul12@users.noreply.github.com>
2022-10-20 17:17:53 +0600

    Add files via upload

---
+++ TimeComplexity merge sort.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#include <chrono>
+using namespace std::chrono;
+using namespace std;
+
+void merge(int arr[], int p, int q, int r) {
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1], M[n2];
+
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[q + 1 + j];
+
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = p;
+
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    int m = l + (r - l) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+  }
+}
+
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << arr[i] << " ";
+  cout << endl;
+}
+
+int main(){
+   int arr[] = {6, 5, 12, 10, 9, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  mergeSort(arr, 0, size - 1);
+
+  cout << "Sorted array: \n";
+  printArray(arr, size);
+
+
+auto start1 = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+
+    /*  ..........Change your algorithm...............*/
+   // int*m=bubbleSort(arr, N);
+    /* .......................................  */
+
+
+    /*.....................Time taken by 1st Algorithm........................*/
+    auto end1 = chrono::high_resolution_clock::now();
+    double time_taken1 =chrono::duration_cast<chrono::nanoseconds>(end1 - start1).count();
+        time_taken1 *= 1e-9;
+    cout << "Time taken by program is : " << fixed
+         << time_taken1 << setprecision(9);
+    cout << " sec" << endl;
+
+
+return 0;
+}
