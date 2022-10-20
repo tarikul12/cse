cfecf43cf86a38764b375279da016f7dbfe5dc2f
tarikul12 <114867607+tarikul12@users.noreply.github.com>
2022-10-20 17:22:41 +0600

    Add files via upload

---
+++ TimeComplexity quick sort.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include <chrono>
+using namespace std::chrono;
+using namespace std;
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i
+        = (low
+           - 1);
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main(){
+     int arr[] = { 10, 7, 8, 9, 1, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+
+auto start1 = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+
+    /*  ..........Change your algorithm...............*/
+   // int*m=bubbleSort(arr, N);
+    /* .......................................  */
+
+
+    /*.....................Time taken by 1st Algorithm........................*/
+    auto end1 = chrono::high_resolution_clock::now();
+    double time_taken1 =chrono::duration_cast<chrono::nanoseconds>(end1 - start1).count();
+        time_taken1 *= 1e-9;
+    cout << "Time taken by program is : " << fixed
+         << time_taken1 << setprecision(9);
+    cout << " sec" << endl;
+
+
+return 0;
+}
